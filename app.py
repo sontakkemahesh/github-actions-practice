bfe1af2630b64bc9a738170a412f5c75b252f222
sontakkemahesh <35212852+sontakkemahesh@users.noreply.github.com>
2026-08-25 20:14:35 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(_name_)
+
+
+@app.route()
+def hello_world():
+  return render_template('index.html')
+
+@app.route('/health')
+def health():
+  return 'server is up and running'
